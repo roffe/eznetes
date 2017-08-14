891e0ef790af1821d53926e3fe91b653bed0bd70
Joakim Karlsson <joakim.karlsson@agillic.com>
2017-08-14 11:56:14 +0200

    refactoring

--- scripts/inc/kube-scheduler.sh
+++ scripts/inc/kube-scheduler.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+
+local TEMPLATE=/etc/kubernetes/scheduler-kubeconfig.yaml
+echo "TEMPLATE: $TEMPLATE"
+mkdir -p $(dirname $TEMPLATE)
+cat << EOF > $TEMPLATE
+apiVersion: v1
+kind: Config
+clusters:
+- name: local
+  cluster:
+    certificate-authority: /etc/kubernetes/ssl/ca.pem
+users:
+- name: kubelet
+  user:
+    client-certificate: /etc/kubernetes/ssl/scheduler.pem
+    client-key: /etc/kubernetes/ssl/scheduler-key.pem
+contexts:
+- context:
+    cluster: local
+    user: kubelet
+  name: kubelet-context
+current-context: kubelet-context
+EOF
+
 local TEMPLATE=/etc/kubernetes/manifests/kube-scheduler.yaml
 echo "TEMPLATE: $TEMPLATE"
 mkdir -p $(dirname $TEMPLATE)
