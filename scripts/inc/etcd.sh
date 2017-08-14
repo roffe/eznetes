65666e8c81e7861c30859b067bdba6125de55a77
Joakim Karlsson <joakim.karlsson@agillic.com>
2017-08-14 13:44:02 +0200

    Updates to instal scripts

---
+++ scripts/inc/etcd.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+local TEMPLATE=/etc/kubernetes/manifests/kube-etcd.yaml
+echo "TEMPLATE: $TEMPLATE"
+mkdir -p $(dirname $TEMPLATE)
+cat << EOF > $TEMPLATE
+apiVersion: v1
+kind: Pod
+metadata:
+  name: kube-etcd
+  namespace: kube-system
+spec:
+  hostNetwork: true
+  containers:
+  - name: kube-etcd
+    image: quay.io/coreos/etcd:v3.2
+    command:
+    - etcd
+    - --config-file=/etc/etcd/etcd.yaml
+    env:
+    - name: GOMAXPROCS
+      value: "2"
+#    livenessProbe:
+#      httpGet:
+#        host: 127.0.0.1
+#        port: 8080
+#        path: /healthz
+#      initialDelaySeconds: 15
+#      timeoutSeconds: 15
+    ports:
+    - containerPort: 2380
+      hostPort: 2380
+      name: peer
+    - containerPort: 2379
+      hostPort: 2379
+      name: client
+    volumeMounts:
+    - mountPath: /etc/kubernetes/ssl
+      name: ssl-certs-kubernetes
+      readOnly: true
+    - mountPath: /etc/ssl/certs
+      name: ssl-certs-host
+      readOnly: true
+    - mountPath: /etc/ssl/etcd
+      name: "etc-ssl-etcd"
+      readOnly: true
+    - mountPath: /etc/etcd/etcd.yaml
+      name: "etcd-conf"
+      readOnly: true
+    - mountPath: /data/etcd
+      name: "data-etcd"
+      readOnly: false
+  volumes:
+  - hostPath:
+      path: /etc/etcd/etcd.yaml
+    name: etcd-conf
+  - hostPath:
+      path: /etc/kubernetes/ssl
+    name: ssl-certs-kubernetes
+  - hostPath:
+      path: "/etc/ssl/etcd"
+    name: "etc-ssl-etcd"
+  - hostPath:
+      path: /usr/share/ca-certificates
+    name: ssl-certs-host
+  - hostPath:
+      path: /data/etcd
+    name: data-etcd
+EOF
+
+
+
+local TEMPLATE=/etc/etcd/etcd.yaml
+echo "TEMPLATE: $TEMPLATE"
+mkdir -p $(dirname $TEMPLATE)
+cat << EOF > $TEMPLATE
+name: '${NODE_HOSTNAME}'
+data-dir: /data/etcd
+wal-dir:
+snapshot-count: 10000
+heartbeat-interval: 100
+election-timeout: 1000
+quota-backend-bytes: 0
+listen-peer-urls: https://0.0.0.0:${ETCD_PEER_PORT}
+listen-client-urls: https://0.0.0.0:${ETCD_CLIENT_PORT}
+max-snapshots: 5
+max-wals: 5
+cors:
+initial-advertise-peer-urls: https://${ADVERTISE_IP}:${ETCD_PEER_PORT}
+advertise-client-urls: https://${ADVERTISE_IP}:${ETCD_CLIENT_PORT}
+discovery:
+discovery-fallback: 'proxy'
+discovery-proxy:
+discovery-srv:
+initial-cluster: $(etcd_initial_cluster_list)
+initial-cluster-token: '${ETCD_CLUSTER_TOKEN}'
+initial-cluster-state: 'new'
+strict-reconfig-check: true
+enable-v2: true
+proxy: 'off'
+proxy-failure-wait: 5000
+proxy-refresh-interval: 30000
+proxy-dial-timeout: 1000
+proxy-write-timeout: 5000
+proxy-read-timeout: 0
+client-transport-security:
+  cert-file: /etc/ssl/etcd/${NODE_HOSTNAME}.pem
+  key-file: /etc/ssl/etcd/${NODE_HOSTNAME}-key.pem
+  client-cert-auth: true
+  trusted-ca-file: /etc/ssl/etcd/ca.pem
+  auto-tls: false
+peer-transport-security:
+  cert-file: /etc/ssl/etcd/${NODE_HOSTNAME}-peer.pem
+  key-file: /etc/ssl/etcd/${NODE_HOSTNAME}-peer-key.pem
+  client-cert-auth: true
+  trusted-ca-file: /etc/ssl/etcd/ca.pem
+  auto-tls: false
+debug: false
+log-package-levels:
+force-new-cluster: false
+EOF
---
+++ scripts/inc/kubelet-etcd.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+local TEMPLATE=/etc/systemd/system/kubelet.service
+local uuid_file="/var/run/kubelet-pod.uuid"
+echo "TEMPLATE: $TEMPLATE"
+mkdir -p $(dirname $TEMPLATE)
+cat << EOF > $TEMPLATE
+[Service]
+Environment=KUBELET_IMAGE_TAG=${K8S_VER}
+Environment=KUBELET_IMAGE_URL=docker://${HYPERKUBE_IMAGE_REPO}
+Environment="RKT_RUN_ARGS=--insecure-options=image \
+  --uuid-file-save=${uuid_file} \
+  --volume dns,kind=host,source=/etc/resolv.conf \
+  --mount volume=dns,target=/etc/resolv.conf \
+  --volume rkt,kind=host,source=/opt/bin/host-rkt \
+  --mount volume=rkt,target=/usr/bin/rkt \
+  --volume var-lib-rkt,kind=host,source=/var/lib/rkt \
+  --mount volume=var-lib-rkt,target=/var/lib/rkt \
+  --volume var-lib-cni,kind=host,source=/var/lib/cni \
+  --mount volume=var-lib-cni,target=/var/lib/cni \
+  --volume stage,kind=host,source=/tmp \
+  --mount volume=stage,target=/tmp \
+  --volume var-log,kind=host,source=/var/log \
+  --mount volume=var-log,target=/var/log \
+  ${CALICO_OPTS}"
+ExecStartPre=/usr/bin/mkdir -p /var/lib/cni
+ExecStartPre=/usr/bin/mkdir -p /etc/kubernetes/manifests
+ExecStartPre=/usr/bin/mkdir -p /var/log/containers
+ExecStartPre=-/usr/bin/rkt rm --uuid-file=${uuid_file}
+ExecStartPre=/usr/bin/mkdir -p /opt/cni/bin
+ExecStart=/usr/lib/coreos/kubelet-wrapper \
+  --api-servers=${CONTROLLER_ENDPOINT} \
+  --cni-conf-dir=/etc/kubernetes/cni/net.d \
+  --network-plugin=cni \
+  --container-runtime=${CONTAINER_RUNTIME} \
+  --rkt-path=/usr/bin/rkt \
+  --rkt-stage1-image=coreos.com/rkt/stage1-fly \
+  --register-with-taints="node-role.kubernetes.io/etcd=:NoSchedule" \
+  --register-node=true \
+  --max-pods=${MAX_PODS} \
+  --allow-privileged=true \
+  --pod-manifest-path=/etc/kubernetes/manifests \
+  --hostname-override=${ADVERTISE_IP} \
+  --cluster-dns=${DNS_SERVICE_IP} \
+  --cluster-domain=cluster.local \
+  --kubeconfig=/etc/kubernetes/etcd-kubeconfig.yaml
+ExecStop=-/usr/bin/rkt stop --uuid-file=${uuid_file}
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+local TEMPLATE=/etc/kubernetes/etcd-kubeconfig.yaml
+echo "TEMPLATE: $TEMPLATE"
+mkdir -p $(dirname $TEMPLATE)
+cat << EOF > $TEMPLATE
+apiVersion: v1
+kind: Config
+clusters:
+- name: local
+  cluster:
+    certificate-authority: /etc/kubernetes/ssl/ca.pem
+users:
+- name: kubelet
+  user:
+    client-certificate: /etc/kubernetes/ssl/${NODE_HOSTNAME}.pem
+    client-key: /etc/kubernetes/ssl/${NODE_HOSTNAME}-key.pem
+contexts:
+- context:
+    cluster: local
+    user: kubelet
+  name: kubelet-context
+current-context: kubelet-context
+EOF
