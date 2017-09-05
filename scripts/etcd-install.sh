9159053fad6298b49146d5b11c696831fd7851fc
Joakim Karlsson <joakim.karlsson@agillic.com>
2017-09-05 16:23:57 +0200

    etcd work

--- scripts/etcd-install.sh
+++ scripts/etcd-install.sh
@@ -2,46 +2,16 @@
 set -e
 
 mkdir -p /etc/ssl/etcd
-#mkdir -p /etc/kubernetes/ssl
 
 mv ssl/client* /etc/ssl/etcd/
 mv ssl/etcd/* /etc/ssl/etcd/
 cp ssl/ca.pem /etc/ssl/etcd/
-
-#mv ssl/* /etc/kubernetes/ssl/
-
 chmod 600 /etc/ssl/etcd/*-key.pem
-#chmod 600 /etc/kubernetes/ssl/*-key.pem
-
-chown root:root /etc/ssl/etcd/*-key.pem
-#chown root:root /etc/kubernetes/ssl/*-key.pem
+chown root:root /etc/ssl/etcd/*-key.pemm
 
 source settings.rc
 
 # -------------
-
-function init_config() {
-	local REQUIRED=('ADVERTISE_IP' 'ETCD_ENDPOINTS' 'CONTROLLER_ENDPOINT' 'DNS_SERVICE_IP' 'K8S_VER' 'HYPERKUBE_IMAGE_REPO' 'USE_CALICO' 'MAX_PODS')
-
-	if [ -z $MAX_PODS ]; then
-		# Number of Pods that can run on this Kubelet. (default 110)
-		export MAX_PODS=110
-	fi
-
-	if [ "${USE_CALICO}" = "true" ]; then
-		export CALICO_OPTS="--volume cni-bin,kind=host,source=/opt/cni/bin \
-                            --mount volume=cni-bin,target=/opt/cni/bin"
-	else
-		export CALICO_OPTS=""
-	fi
-
-	for REQ in "${REQUIRED[@]}"; do
-		if [ -z "$(eval echo \$$REQ)" ]; then
-			echo "Missing required config value: ${REQ}"
-			exit 1
-		fi
-	done
-}
 
 function etcd_initial_cluster_list() {
 	local arr=$(echo -n ${ETCD_ENDPOINTS} | tr "," "\n")
@@ -54,14 +24,13 @@ function etcd_initial_cluster_list() {
 }
 
 function init_templates() {
-	source inc/docker.sh
-	source inc/kube-etcd.sh
+#	source inc/docker.sh
+	source inc/etcd.sh
 #	source inc/rkt.sh
 #	source inc/kubelet-etcd.sh
 #	source inc/kube-proxy.sh
 }
 
-init_config
 init_templates
 
 # chmod +x /opt/bin/host-rkt
@@ -69,20 +38,7 @@ init_templates
 echo "Running Daemon reload"
 systemctl daemon-reload
 
-if [ $CONTAINER_RUNTIME = "rkt" ]; then
-	echo "Load rkt stage1 images"
-	systemctl enable load-rkt-stage1
-	echo "Enable rkt-api"
-	systemctl enable rkt-api
-fi
-
-echo "Restarting Flannel"
-systemctl enable flanneld
-systemctl restart flanneld
-
-echo "Restarting Kubelet"
-systemctl enable kubelet
-systemctl restart kubelet
+docker run -d --restart=always --net=host -v /data/etcd:/data/etcd -v /etc/etcd/etcd.yaml:/etc/etcd/etcd.yaml -v /etc/ssl/etcd:/etc/ssl/etcd  --name=etcd quay.io/coreos/etcd:v3.2 etcd --config-file=/etc/etcd/etcd.yaml
 
 echo "**You must SSH to the node(s) and change initial-cluster-state: 'new' to initial-cluster-state: 'existing' in /etc/etcd/etcd.yaml once initial cluster state is reached for restarts of ETCD to work properly**"
 echo "DONE"
