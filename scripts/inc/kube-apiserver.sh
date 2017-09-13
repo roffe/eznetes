ea0c20e9792ca403df9465e06945640daf76de99
Joakim Karlsson <joakim.karlsson@agillic.com>
2017-09-13 12:49:36 +0200

    enabled batch/v2alpha1=true in apiserver to have cron jobs

--- scripts/inc/kube-apiserver.sh
+++ scripts/inc/kube-apiserver.sh
@@ -34,7 +34,7 @@ spec:
     - --tls-private-key-file=/etc/kubernetes/ssl/apiserver-${NODE_HOSTNAME}-key.pem
     - --client-ca-file=/etc/kubernetes/ssl/ca.pem
     - --service-account-key-file=/etc/kubernetes/ssl/controller-key.pem
-    - --runtime-config=extensions/v1beta1/networkpolicies=true
+    - --runtime-config=extensions/v1beta1/networkpolicies=true,batch/v2alpha1=true
     - --anonymous-auth=false
     - --experimental-bootstrap-token-auth
     - --runtime-config=authentication.k8s.io/v1beta1=true
