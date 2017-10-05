cbaa59952e48a3d168c4229d3e5b47dd9afb43b7
Joakim Karlsson <joakim@roffe.nu>
2017-10-05 11:31:45 +0200

    added HOME env to RKT

--- scripts/inc/kubelet-worker.sh
+++ scripts/inc/kubelet-worker.sh
@@ -51,7 +51,7 @@ Environment="RKT_RUN_ARGS=--insecure-options=image \
   --mount volume=var-log,target=/var/log \
   --volume etc-cni,kind=host,source=/etc/cni \
   --mount volume=etc-cni,target=/etc/cni \
-  --set-env=HOME=/root \
+  --environment=HOME=/root \
   ${CNI_OPTS}"
 ExecStartPre=/usr/bin/mkdir -p /var/lib/cni
 ExecStartPre=/usr/bin/mkdir -p /etc/kubernetes/manifests
