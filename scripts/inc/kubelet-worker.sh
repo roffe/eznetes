ec03b61578b6617c516303ee7a56331a2ca31e3e
Joakim Karlsson <joakim@roffe.nu>
2017-10-05 11:47:00 +0200

    added HOME env to RKT

--- scripts/inc/kubelet-worker.sh
+++ scripts/inc/kubelet-worker.sh
@@ -33,7 +33,8 @@ cat <<EOF >$TEMPLATE
 [Service]
 Environment=KUBELET_IMAGE_TAG=${K8S_VER}
 Environment=KUBELET_IMAGE_URL=docker://${HYPERKUBE_IMAGE_REPO}
-Environment="KUBELET_IMAGE_ARGS=--environment=home=/root"
+Environment="KUBELET_IMAGE_ARGS=--environment=home=/root \
+  --exec=/kubelet"
 Environment="RKT_RUN_ARGS=--insecure-options=image \
   --uuid-file-save=${uuid_file} \
   --volume dns,kind=host,source=/etc/resolv.conf \
